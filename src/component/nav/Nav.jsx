f0002e35a85a535afa5e09c15cfbcb1c044b3099
mason park <joung4342@gmail.com>
2023-08-09 20:21:06 +0900

    Nav 컴포넌트 스타일링

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -5,15 +5,15 @@ import  {Link} from 'react-router-dom'
 function Nav(props) {
 
     return (
-        <header>
-            <Link to='/'>
+        <header className='flex justify-between border-b border-gray-300 p-2'>
+            <Link className='flex items-center text-4xl text-brand' to='/'>
                 <BiShoppingBag/>
                 <h1>My Shop</h1>
             </Link>
-            <nav>
+            <nav className='flex items-center gap-4 font-semibold'>
                 <Link to='/products'>Products</Link>
                 <Link to='/carts'>Carts</Link>
-                <Link to='/products/new'><FaPencil/></Link>
+                <Link to='/products/new' className='text-2xl'><FaPencil/></Link>
                 <button>Login</button>
             </nav>
 
