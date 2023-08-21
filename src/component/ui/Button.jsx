2a38801bc461b864b804dce55ddaf58515595c0c
mason park <joung4342@gmail.com>
2023-08-21 20:13:28 +0900

    NewProduct Component UI Update

--- src/component/ui/Button.jsx
+++ src/component/ui/Button.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Button({text,onClick,isLoading}) {
     return (
-        <div className='bg-brand text-white p-2 rounded-sm hover:brightness-110 w-32 text-center'>
-            {isLoading && <div className='fixed opacity-75 animate-spin mx-1 w-6 h-6 border-[3px] border-current border-t-transparent text-gray-800 rounded-full dark:text-white'/>}
-            <button onClick={onClick} className='w-full h-full'>{text}</button>
+        <div className='bg-brand text-white p-2 rounded-sm hover:brightness-110 text-center'>
+            {isLoading && <div className='fixed end-1/2 opacity-75 animate-spin mx-1 w-6 h-6 border-[3px] border-current border-t-transparent text-gray-800 rounded-full dark:text-white'/>}
+            <button onClick={onClick} disabled={isLoading} className='w-full h-full'>{text}</button>
         </div>
 
     );
--- src/page/newProduct/NewProduct.jsx
+++ src/page/newProduct/NewProduct.jsx
@@ -39,11 +39,11 @@ function NewProduct(props) {
         setProducts((products) => ({...products, [name]: value, files}));
     };
     return (
-        <section>
-            <h2>새로운 제품 등록</h2>
-            {success && <p>✅{success}</p>}
-            {file && <img src={URL.createObjectURL(file)} alt="upload File"/>}
-            <form onSubmit={handleSubmit}>
+        <section className='w-full text-center'>
+            <h2 className='text-2xl font-bold my-4'>새로운 제품 등록</h2>
+            {success && <p className='my-2'>✅{success}</p>}
+            {file && <img className='w-96 m-auto mb-2' src={URL.createObjectURL(file)} alt="upload File"/>}
+            <form  className='flex flex-col px-12' onSubmit={handleSubmit}>
                 <input type="file" accept='image/*' name='file' required onChange={handleChange}/>
                 <input type="text" name='title' value={products.title ?? ''} placeholder='제품명' required
                        onChange={handleChange}/>
