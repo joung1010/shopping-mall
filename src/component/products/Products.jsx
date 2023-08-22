11cf206e40b88fa237c5dfd9b2d8e8681b420bd4
mason park <joung4342@gmail.com>
2023-08-22 20:56:18 +0900

    Product Page UI update

--- src/component/products/Products.jsx
+++ src/component/products/Products.jsx
@@ -15,7 +15,7 @@ function Products(props) {
         <>
             {isLoading && <p>Loading...</p>}
             {error && <p>{error}</p>}
-            <ul>
+            <ul className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4'>
                 {products &&
                 products.map(product => <ProductCard key={product.id} product={product}/>)
                 }
