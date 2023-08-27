f168ed51a0c6b62b6dbd38e6a755202e805f23e1
mason park <joung4342@gmail.com>
2023-08-28 08:58:05 +0900

    Update ProductDetail

--- src/page/productDetail/ProductDetail.jsx
+++ src/page/productDetail/ProductDetail.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useLocation, } from "react-router-dom";
+import Button from "../../component/ui/Button";
 
 function ProductDetail(props) {
+    const {state:{product:{image,title,category,price,description,options}}} =useLocation();
+    const [selected, setSelected] = useState(options && options[0]);
+    const handleChange = (e) => {
+        setSelected(e.target.value);
+    };
+    const handleClick = (e) => {
+
+    };
     return (
-        <div></div>
+        <>
+            <p className='mx-12 mt-4 text-gray-700'>{category}</p>
+            <section className='flex flex-col md:flex-row p-4'>
+                <img className='w-full px-4 basis-7/12' src={image} alt={title}/>
+                <div className='w-full basis-5/12 flex flex-col p-4'>
+                    <h2 className='text-3xl font-bold py-2'>{title}</h2>
+                    <p className='text-2xl font-bold py-2 border-b border-gray-400'>${price}</p>
+                    <p className='py-4 text-lg'>{description}</p>
+                   <div className='flex items-center mb-2'>
+                       <label className='text-brand font-bold' htmlFor='select'>옵션:</label>
+                       <select className='p-2 m-3 border-2 flex-1 border-dashed border-brand outline-none' onChange={handleChange} value={selected}>
+                           {options &&
+                           options.map((option,idx) => (<option key={idx} value={option}>{option}</option>))
+
+                           }
+                       </select>
+                   </div>
+                    <Button text='장바구니 추가' onClick={handleClick}/>
+                </div>
+            </section>
+
+        </>
     );
 }
 
--- src/productCard/ProductCard.jsx
+++ src/productCard/ProductCard.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 
-function ProductCard({product:{id,image,title,category,price}}) {
+function ProductCard({product,product:{id,image,title,category,price}}) {
+    const navigate = useNavigate();
+    const handleOnClick = (e) => {
+        e.preventDefault();
+        navigate(`/products/${id}`, {state: {product,}});
+    };
     return (
-        <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
+        <li onClick={handleOnClick}
+            className='rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105'
+        >
             <img src={image} alt={title} className='w-full'/>
             <div className='mt-2 px-2 text-lg flex justify-between items-center'>
                 <h3 className='truncate'>{title}</h3>
